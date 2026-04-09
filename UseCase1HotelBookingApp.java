326fdccbc7290fc47eea76010190bfb804787ec8
Adarsh <adarshsr221073@gmail.com>
2026-04-09 14:38:30 +0530

    Implement Use Case 1

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+/**
+ * Application Entry point for Book My Stay App.
+ * @author BookMyStay
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Welcome to the Hotel Booking Application!");
+    }
+}
